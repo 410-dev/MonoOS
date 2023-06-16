30307ac093833dbe4078484555c199addc958852
410 <58301450+410-dev@users.noreply.github.com>
2023-06-16 04:26:10 -0400

    KernelInfo: Loads from NVRAM and ServicesManager

--- src/main/java/org/mono/kernel/kernel/KernelInfo.java
+++ src/main/java/org/mono/kernel/kernel/KernelInfo.java
@@ -2,6 +2,8 @@ package org.mono.kernel.kernel;
 
 import com.google.gson.JsonObject;
 import me.hysong.libhyextended.utils.JsonBeautifier;
+import org.mono.kernel.NVRAM;
+import org.mono.kernel.ServicesManager;
 
 public class KernelInfo {
     public static String main() {
@@ -14,13 +16,13 @@ public class KernelInfo {
         kernel.addProperty("author", "410-dev");
         kernel.addProperty("license", "Undefined");
 
-        kernelModules.addProperty("bootloader", "Built-in STG2 bootloader");
-        kernelModules.addProperty("init", "ServicesManager");
-        kernelModules.addProperty("frameworks", "ServicesManager");
-        kernelModules.addProperty("shell", "Shell");
-        kernelModules.addProperty("ui", "BasicShellUI");
-        kernelModules.addProperty("nvram", "On-FS NVRAM");
-        kernelModules.addProperty("environment", "Environment");
+        kernelModules.addProperty("bootloader", NVRAM.get("BOOTLOADER_NAME") + " " + NVRAM.get("BOOTLOADER_VERS"));
+        kernelModules.addProperty("init", ServicesManager.getServiceByType("dummy-init").getName());
+        kernelModules.addProperty("framework", ServicesManager.getServiceByType("dummy-framework").getName());
+        kernelModules.addProperty("shell", ServicesManager.getServiceByType("shell").getName());
+        kernelModules.addProperty("ui", ServicesManager.getServiceByType("ui").getName());
+        kernelModules.addProperty("nvram", ServicesManager.getServiceByType("dummy-nvram").getName());
+        kernelModules.addProperty("environment", ServicesManager.getServiceByType("dummy-env").getName());
         kernel.add("modules", kernelModules);
 
         system.addProperty("name", "MonoOS");
